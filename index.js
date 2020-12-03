41abd8098cc01df0e094fa1c320d50d7ff8a728d
gobelinmalin <mariemeline.paus@gmail.com>
2020-12-03 12:29:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 const dotenv = require('dotenv');
 const cors = require('cors');
 const routes = require('./routes');
